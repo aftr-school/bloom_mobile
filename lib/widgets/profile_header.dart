7416b010c77afe14199780ff4dd4bb6504fa005a
Reza Ageng <rezaageng5@gmail.com>
2022-08-27 07:51:46 +0700

    style(profile): :lipstick: update profile header layout

--- lib/widgets/profile_header.dart
+++ lib/widgets/profile_header.dart
@@ -7,7 +7,7 @@ class ProfileHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(16),
-      child: Row(
+      child: Column(
         children: [
           const CircleAvatar(
             foregroundImage: NetworkImage(
@@ -15,34 +15,36 @@ class ProfileHeader extends StatelessWidget {
             ),
             radius: 46,
           ),
-          Flexible(
-            child: Container(
-              margin: const EdgeInsets.only(left: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Your Maple Leaf',
-                    style: Theme.of(context).textTheme.headline6,
+          Container(
+            margin: const EdgeInsets.only(top: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Your Maple Leaf',
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.secondary,
+                    padding: EdgeInsets.zero,
+                    minimumSize: const Size(0, 0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    splashFactory: NoSplash.splashFactory,
+                    enableFeedback: true,
+                  ),
+                  child: const Text(
+                    'Jl. katapang menuju soreang km 18',
+                    textAlign: TextAlign.center,
                     overflow: TextOverflow.ellipsis,
                   ),
-                  const Text('Farmer'),
-                  TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                      primary: Theme.of(context).colorScheme.secondary,
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(0, 0),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      splashFactory: NoSplash.splashFactory,
-                      enableFeedback: true,
-                    ),
-                    child: const Text('Adress'),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
